2f37271f90fdc4659a4a6b163505d6c6593e8efb
kishor <kishorwadkar1717@gmail.com>
2023-07-04 13:28:14 +0530

    this is first commit

---
+++ src/Pages/LoginPage.java
@@ -0,0 +1,16 @@
+package Pages;
+
+public class LoginPage 
+{
+	public void enterusername()
+	{
+		System.out.println("enter the username");
+	}
+	public void enterPassword()
+	{
+		System.out.println("enter the password");
+	}
+
+	
+
+}
